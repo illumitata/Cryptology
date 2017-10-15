c6ed03acb5f1d241a78a2e129c654ea88a56241c
illumitata <gothic8282@gmail.com>
2017-10-15 17:45:38 +0200

    getting started :

--- Caesar_Affine_Cipher/main.c
+++ Caesar_Affine_Cipher/main.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+
+int main (int argc, char **argv){
+
+  if(!strcmp(argv[1], "-c") || !strcmp(argv[1], "-a")){
+    //Ceaser section
+    if(!strcmp(argv[1], "-c") && !strcmp(argv[2], "-e")){
+      //szyfrowanie cezarem
+    }
+    if(!strcmp(argv[1], "-c") && !strcmp(argv[2], "-d")){
+      //odszyfrowanie cezarem
+    }
+    if(!strcmp(argv[1], "-c") && !strcmp(argv[2], "-j")){
+      //kryptoanaliza z tekstem jawnym cezarem
+    }
+    if(!strcmp(argv[1], "-c") && !strcmp(argv[2], "-k")){
+      //kryptoanaliza w oparciu o kryptogram cezarem
+    }
+    /////////////////
+    //Affine section
+    if(!strcmp(argv[1], "-a") && !strcmp(argv[2], "-e")){
+      //szyfrowanie afinicznym
+    }
+    if(!strcmp(argv[1], "-a") && !strcmp(argv[2], "-d")){
+      //odszyfrowanie afinicznym
+    }
+    if(!strcmp(argv[1], "-a") && !strcmp(argv[2], "-j")){
+      //kryptoanaliza z tekstem jawnym afinicznym
+    }
+    if(!strcmp(argv[1], "-a") && !strcmp(argv[2], "-k")){
+      //kryptoanaliza w oparciu o kryptogram aficzninym
+    }
+  }
+  else printf("You entered incorrect options! \n");
+
+  return 0;
+}
